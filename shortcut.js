d8c9a11bce92d81ab40f624edcc5d240cb826c31
shoieb-alam <86704871+shoieb-alam@users.noreply.github.com>
2021-09-12 12:48:46 +0600

    shrotcut updated

--- shortcut.js
+++ shortcut.js
@@ -10,12 +10,7 @@ else {
     myVar = 0;
 }
 
-let myMoney = 50;
-// you check negative or falsy anything
-if (!myMoney) {
-
-}
-
+// check negative or falsy anything
 const money = 800;
 let food;
 if (money > 100) {
@@ -25,29 +20,29 @@ else {
     food = 'cha biscuit';
 }
 
-// ternary
+// ternary or shortcut for previous if-else
 let food1 = money > 100 ? 'biryani' : 'cha biscuit';
-// console.log(food1);
+console.log(food1);
 
 let drink = (money > 100 && myVar > 100) ? 'coke' : 'filter water';
-// console.log(drink);
+console.log(drink);
 
 // number to string conversion
 const num1 = 52;
-// console.log(num1);
+console.log(num1);
 const numStr = num1 + '';
-// console.log(numStr)
+console.log(numStr)
 
 // string to number
 const input = '560';
 const inputNum = +input;
-// console.log(inputNum);
+console.log(inputNum);
 
 // 
 let isActive = true;
 const showUser = () => console.log('display User');
 const hideUser = () => console.log('hide User');
-// isActive ? showUser() : hideUser();
+isActive ? showUser() : hideUser();
 // use && if the left side is true then right side will be executed
 isActive && showUser();
 // use || if the left side is false then right side will be executed
